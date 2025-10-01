67b93b41c3ae3bd6fe041e1e899be65489b50418
0xChijioke <chijokeogbuanya@gmail.com>
2025-10-01 10:26:13 +0100

    wagmi config

---
+++ services/wagmi.ts
@@ -0,0 +1,29 @@
+import { http, createConfig } from "wagmi";
+import { sepolia, mainnet, hardhat, arbitrum, gnosis } from "wagmi/chains";
+import { injected, walletConnect } from "wagmi/connectors";
+
+export const config = createConfig({
+  chains: [sepolia, mainnet, arbitrum, gnosis],
+  connectors: [
+    injected(),
+    walletConnect({
+      projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || "",
+    }),
+  ],
+  transports: {
+    [sepolia.id]: http(),
+    [arbitrum.id]: http(),
+    [mainnet.id]: http(),
+    [gnosis.id]: http(),
+  },
+});
+
+export function getConfig() {
+  return config;
+}
+
+declare module "wagmi" {
+  interface Register {
+    config: ReturnType<typeof getConfig>;
+  }
+}
